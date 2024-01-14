cad75f2f01493033427cae731b62c737297f724d
Tatiana Korotkova <acerbity@mail.ru>
2024-01-14 16:10:19 +0300

    revised hexlet tests 5

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,10 +3,10 @@ import game from '../index.js';
 const brainPrime = () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const gameFunction = () => {
-    const question = Math.round(Math.random() * 100);
+    let question = Math.round(Math.random() * 100);
     let correctAnswer = '';
     let result = 0;
-    for (let x = 1; x <= question; x=+1) {
+    for (let x = 1; x <= question; x++) {
       if (question % x === 0) {
         result += 1;
       }
