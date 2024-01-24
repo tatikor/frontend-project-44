40b53ec66114231b3d176c53f40b3f337e85fe6f
Tatiana Korotkova <acerbity@mail.ru>
2024-01-24 21:12:42 +0300

    fix according to comments 2-4

--- src/more_functions.js
+++ src/more_functions.js
@@ -1,6 +1,5 @@
 const getRandomNumber = () => {
-    const number = Math.round(Math.random() * 100);
-    return number;
-  };
-  
+  const number = Math.round(Math.random() * 100);
+  return number;
+};
   export default getRandomNumber;
